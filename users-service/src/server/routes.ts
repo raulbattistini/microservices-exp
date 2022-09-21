b3675993a70c8630349b99e31325fa84883907db
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-21 16:55:43 -0300

    adding: new route

--- users-service/src/server/routes.ts
+++ users-service/src/server/routes.ts
@@ -5,6 +5,23 @@ import User from "../models/User";
 
 export const setRoutes = (app: Express) => {
   const userRepository = AppDataSource.getRepository(User);
+
+  app.post("/sessions", async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      return next(new Error("Invalid body!"));
+    }
+    try {
+      const user = await userRepository.findBy({
+        username: req.body.username,
+        select: ["id", "passwordHash"]
+        
+      }
+      );
+    } catch (err) {
+      return next(err);
+    }
+  });
+
   app.get("/users/:id", async (req: Request, res, next) => {
     try {
       const user = await userRepository.findOne({
@@ -12,7 +29,7 @@ export const setRoutes = (app: Express) => {
       });
 
       if (req.params.id === null) {
-        return res.status(404).json({message: "Sorry can't find that!"});
+        return res.status(404).json({ message: "Sorry can't find that!" });
       }
       return res.json(user);
     } catch (err) {
