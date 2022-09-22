9a6ff48ada25e5e3bc8e67ea1a65f3b7ee01c4ab
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-22 00:39:03 -0300

    fix

--- users-service/src/server/startServer.ts
+++ users-service/src/server/startServer.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import accessEnv from "../helpers/helper";
@@ -10,7 +9,7 @@ const PORT = parseInt(accessEnv("PORT", "3333"), 10);
 export const startServer = () => {
   const app = express();
 
-  app.use(bodyParser.json());
+  app.use(express.json());
 
   app.use(
     cors({
@@ -25,7 +24,7 @@ export const startServer = () => {
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     return res.status(500).json({ message: err.message });
   });
-
+  
   app.listen(PORT, ()=>{
    console.log(`Server is listening on port: ${PORT}`)
   })
