3db148c930cf7932fe284953e9a3be733b8f4b2b
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-25 06:57:08 -0300

    add: routes

---
+++ chat-app/src/pages/Home/index.tsx
@@ -0,0 +1,46 @@
+import { gql } from "@apollo/client";
+import React, { useEffect, useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import { apolloClient } from "../../api/apolloClient";
+import { useRecoilState } from "recoil";
+import { userSessionAtom } from "../../recoil/atoms/userSession";
+
+
+const query = gql`
+  {
+    userSession(me: true) {
+      user {
+        username
+      }
+    }
+  }
+`;
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+export const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userSession, setUserSession] = useRecoilState(userSessionAtom);
+  let [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    apolloClient.query({ query }).then((res) => {
+      const userSession = res.data?.userSession ?? null;
+
+      setUserSession(userSession);
+
+      setIsLoading(false);
+    });
+  }, []);
+
+  return isLoading ? (
+    <ClipLoader color={color} cssOverride={override} size={150} />
+  ) : (
+    <>
+      <h1>{JSON.stringify(userSession, null, 2)}</h1>
+    </>
+  );
+};
